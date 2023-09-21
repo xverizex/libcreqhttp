e33f505a1d2bf36c17d9a889e5194774ea377da1
Dmitrii <naidv88@gmail.com>
2023-09-21 08:56:51 +0500

    [+] add license

--- include/creqhttp.h
+++ include/creqhttp.h
@@ -1,3 +1,20 @@
+/*
+ *  libcreqhttp - request get post inputer.
+ *  Copyright (C) 2023 Dmitrii Naidolinskii
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef CREQ_HTTP_H
 #define CREQ_HTTP_H
 
